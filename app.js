3caac01df0d5707a672e1069e117afc2b26dc992
ruchik02 <ruchika02>
2022-04-02 15:48:13 +0530

    change in app.js

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
 const weatherApi={
-<<<<<<< HEAD
     key:weatherApikey,
-=======
-    key:"4926f8f088814d128c6f11d94facfe8a",
-    
->>>>>>> f62ea08ca61e351e59d87f1a8d159fb95fbbf699
     baseUrl:"https://api.openweathermap.org/data/2.5/weather"
 }
 // fetch by id
